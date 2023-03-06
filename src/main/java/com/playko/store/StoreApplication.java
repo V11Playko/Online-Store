c9330bc51a44b345fcf0c8d7bdbc4587f1eef2fc
Heinner Vega <heinnervega20@gmail.com>
2023-03-06 16:09:00 -0500

    first commit

---
+++ src/main/java/com/playko/store/StoreApplication.java
@@ -0,0 +1,15 @@
+package com.playko.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+
+}
