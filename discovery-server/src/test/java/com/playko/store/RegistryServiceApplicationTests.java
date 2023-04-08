5d71635f014a618e0bfda19f64f81fcf3fb118eb
Heinner Vega <heinnervega20@gmail.com>
2023-04-08 18:52:19 -0500

    Add discovery-server

---
+++ discovery-server/src/test/java/com/playko/store/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playko.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
