f2c8a61750792213282d922173abf96d18da023c
Heinner Vega <heinnervega20@gmail.com>
2023-04-10 13:07:27 -0500

    Change, se hicieron los cambios para poder levantar los 3 microservicios

--- customer-service/src/main/java/com/playko/store/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/playko/store/CustomerServiceApplication.java
@@ -2,7 +2,11 @@ package com.playko.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
+@EnableFeignClients
 @SpringBootApplication
 public class CustomerServiceApplication {
 	public static void main(String[] args) {
